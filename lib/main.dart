132bd5094312de9b06a1d60cd333c29929c305f8
Gerardo Villegas <villegasgerardo.j@gmail.com>
2020-09-17 20:25:53 -0500

    Additing chat page

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Chat App',
-        initialRoute: 'users',
+        initialRoute: 'chat',
         routes: appRoutes,
         theme: ThemeData(
           pageTransitionsTheme: PageTransitionsTheme(builders: {
--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -4,8 +4,47 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Chat Page'),
+      appBar: AppBar(
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Column(
+          children: [
+            CircleAvatar(
+              backgroundColor: Colors.blue[100],
+              maxRadius: 15,
+              child: Text(
+                'Te',
+                style: TextStyle(fontSize: 12),
+              ),
+            ),
+            SizedBox(
+              height: 3,
+            ),
+            Text(
+              'John Doe',
+              style: TextStyle(color: Colors.black87, fontSize: 12),
+            )
+          ],
+        ),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Flexible(
+                child: ListView.builder(
+                    reverse: true,
+                    physics: BouncingScrollPhysics(),
+                    itemBuilder: (_, i) => Text('$i'))),
+            Divider(
+              height: 1,
+            ),
+            Container(
+              color: Colors.white,
+              height: 100,
+            )
+          ],
+        ),
       ),
     );
   }
